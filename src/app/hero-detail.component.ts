bfc9d0b63c74b5ef0bc261756210a7052573f695
bringtree <bringtree@qq.com>
2017-05-12 01:33:34 +0800

    beta

---
+++ src/app/hero-detail.component.ts
@@ -0,0 +1,23 @@
+/**
+ * Created by huangpeisong on 2017/5/11.
+ */
+import {Component, Input} from '@angular/core';
+import {Hero} from './hero';
+
+@Component({
+  selector: 'hero-detail',
+  template: `
+    <div *ngIf="hero">
+      <h2>{{hero.name}} details!</h2>
+      <div><label>id:</label>{{hero.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="hero.name" placeholder="name"/>
+      </div>
+    </div>
+  `
+})
+
+export class HeroDetailComponent {
+  @Input() hero: Hero;
+}
